8c116f71be0926b07e42a11c0cd4465d38db042a
TheAkylino <40051292+TheAkylino@users.noreply.github.com>
2022-04-28 21:01:12 -0500

    developmental progress
    developmental progress

--- src/main/java/com/example/models/entity/ExchangeRate.java
+++ src/main/java/com/example/models/entity/ExchangeRate.java
@@ -15,23 +15,24 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+
 @Table(name = "EXCHANGE_RATE")
-@ApiModel(description="Todo los detalles del tipo de cambio")
+@ApiModel(description = "Todo los detalles del tipo de cambio")
 public class ExchangeRate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @ApiModelProperty(notes = "ID del tipo de cambio")
     private Integer id;
 
-    @Column(name = "CODE_ISO_ORIGIN_CURRENCY" ,nullable = false)
+    @Column(name = "CODE_ISO_ORIGIN_CURRENCY", nullable = false)
     @ApiModelProperty(notes = "Codigo iso de la moneda de origen")
     private String codeOriginCurrency;
 
-    @Column(name = "CODE_ISO_DESTINATION_CURRENCY" ,nullable = false)
+    @Column(name = "CODE_ISO_DESTINATION_CURRENCY", nullable = false)
     @ApiModelProperty(notes = "Codigo iso de la moneda de destino")
     private String codeDestinationCurrency;
 
-    @Column(name = "AMOUNT" ,nullable = false)
+    @Column(name = "AMOUNT", nullable = false)
     @ApiModelProperty(notes = "Monto del tipo de cambio")
     private BigDecimal amount;
 }
